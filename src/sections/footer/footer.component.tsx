c18fc5a420140f28da3a1d4b3fa69956112e403e
stelko <stilyanmtoskov@gmail.com>
2023-04-12 00:42:51 +0100

    cta

---
+++ src/sections/footer/footer.component.tsx
@@ -0,0 +1,10 @@
+import CTA from './components/cta/cta.component';
+
+export default function Footer() {
+  return (
+    <div>
+      <CTA />
+      Footer
+    </div>
+  );
+}
--- src/sections/main/main.component.tsx
+++ src/sections/main/main.component.tsx
@@ -4,7 +4,7 @@ import FAQ from './components/faq/faq.component';
 
 export default function Main() {
   return (
-    <main className="mx-8 flex flex-col gap-[140px]">
+    <main className="mx-8 mb-20 flex flex-col gap-[140px]">
       <Hero />
       <Features />
       <FAQ />
